0497b00bb625984edac05e9b05dfce6d7033888b
Pete Walker <pete.walker@me.com>
2016-08-19 11:49:01 +0100

    Removed redundant imports

--- components/charts/charts.ts
+++ components/charts/charts.ts
@@ -2,15 +2,14 @@ import {
   Component, OnDestroy, OnInit, OnChanges, EventEmitter, ElementRef, Input,
   Output
 } from '@angular/core';
-import {CORE_DIRECTIVES, NgClass} from '@angular/common';
-import {FORM_DIRECTIVES} from '@angular/forms';
+import {NgClass} from '@angular/common';
 
 declare var Chart:any;
 
 @Component({
   selector: 'base-chart',
   template: `<canvas style="width: 100%; height: 100%;"></canvas>`,
-  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass],
+  directives: [NgClass],
   styles: [`:host { display: block; }`]
 })
 export class BaseChartComponent implements OnDestroy, OnChanges, OnInit {
